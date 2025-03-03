2aa615f4801f0fc3732a1e4af41d931d373924e7
Wan-Hua Hsieh <wan-hua.hsieh@ucdconnect.ie>
2025-03-03 18:32:35 +0000

    Add API route to provide Google Maps API key

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,12 +1,13 @@
-from flask import Blueprint
-
 # Import individual route modules
 from .bikes import bikes_bp
 from .weather import weather_bp
 from .stations import stations_bp
+from .config import config_bp
+
 
 # Define a function to register Blueprints
 def register_blueprints(app):
     app.register_blueprint(bikes_bp, url_prefix="/api")
     app.register_blueprint(weather_bp, url_prefix="/api")
     app.register_blueprint(stations_bp, url_prefix="/api")
+    app.register_blueprint(config_bp, url_prefix="/api")
