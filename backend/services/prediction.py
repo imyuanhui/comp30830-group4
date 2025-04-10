ebcc5beb3d9269bee332cdc984a1da391991ea2a
YuanhuiXu <xuuuyuanhui@foxmail.com>
2025-04-10 11:10:42 +0100

    feature: add encoded mapping to the machine leanring

--- backend/services/prediction.py
+++ backend/services/prediction.py
@@ -3,20 +3,29 @@ import datetime
 import pandas as pd
 import os
 
-# Path to the model file
+# Path to the model files
 BIKE_MODEL_FILENAME = 'bike_availability_model.pkl'
 BIKE_MODEL_PATH = os.path.join(os.getcwd(), "machine_learning", BIKE_MODEL_FILENAME)
 STAND_MODEL_FILENAME = 'stand_availability_model.pkl'
 STAND_MODEL_PATH = os.path.join(os.getcwd(), "machine_learning", STAND_MODEL_FILENAME)
+BIKE_ENCODED_PATH = os.path.join(os.getcwd(), "machine_learning", "station_bike_encoding.pkl")
+STAND_ENCODED_PATH = os.path.join(os.getcwd(), "machine_learning", "station_stand_encoding.pkl")
 
 # Global variable to store the model in memory
 bike_model = None
 stand_model = None
 
-# Load models only once and keep it in memory
+# Global variable to store encoded mappings in memory
+mean_bike_encoded = None
+mean_stand_encoded = None
+
+# Load models and mappings only once and keep it in memory
 def load_model():
     global bike_model
     global stand_model
+    global mean_bike_encoded
+    global mean_stand_encoded
+
     if bike_model is None:
         with open(BIKE_MODEL_PATH, "rb") as f:
             bike_model = pickle.load(f)
@@ -25,6 +34,17 @@ def load_model():
         with open(STAND_MODEL_PATH, "rb") as f:
             stand_model = pickle.load(f)
         print("Stand Model loaded successfully.")
+    
+    if mean_bike_encoded is None:
+        with open(BIKE_ENCODED_PATH, "rb") as f:
+            mean_bike_encoded = pickle.load(f)
+        print("Bike encoded mapping loaded successfully.")
+    
+    if mean_stand_encoded is None:
+        with open(BIKE_ENCODED_PATH, "rb") as f:
+            mean_stand_encoded = pickle.load(f)
+        print("Stand encoded mapping loaded successfully.")
+
     return bike_model, stand_model
 
 # Feature extraction function
@@ -41,18 +61,26 @@ def predict_availability(station_id, timestamp, temp, target="bike"):
     # load_model()
     global bike_model
     global stand_model
+    global mean_bike_encoded
+    global mean_stand_encoded
 
     # Extract features
     day_of_week, hour = extract_features(timestamp)
 
-    # Prepare input data for model
-    input_data = pd.DataFrame([[station_id, temp, hour, day_of_week]],
-                            columns=['station_id','max_air_temperature_celsius', 'hour', 'day_of_week'])
-
     # Make prediction
     if target == "bike":
+        # Convert station_id to encoded value
+        station_id_encoded = mean_bike_encoded.get(station_id, mean_bike_encoded.mean())
+        # Use encoded station_id in input features
+        input_data = pd.DataFrame([[station_id_encoded, temp, hour, day_of_week]],
+                          columns=['station_id_encoded1', 'max_air_temperature_celsius', 'hour', 'day_of_week'])
         prediction = bike_model.predict(input_data)[0]
     elif target == "stand":
+        # Convert station_id to encoded value
+        station_id_encoded = mean_stand_encoded.get(station_id, mean_stand_encoded.mean())
+         # Use encoded station_id in input features
+        input_data = pd.DataFrame([[station_id_encoded, temp, hour, day_of_week]],
+                          columns=['station_id_encoded2', 'max_air_temperature_celsius', 'hour', 'day_of_week'])
         prediction = stand_model.predict(input_data)[0]
     else:
         raise ValueError("Invalid target specified. Must be 'bike' or 'stand'.")
